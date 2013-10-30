546f11319373feca26abf8642a48360c16d1f54e
Carl Mönnig <carl.monnig@gmail.com>
2013-10-30 07:40:05 +0200

    Fixed English

--- XmlLite.pas
+++ XmlLite.pas
@@ -2,7 +2,7 @@
   Header translation for Microsoft XmlLite
   @author Carl Mönnig
 
-  * XmlLite is a native C++ implementation of .NET XmlReader+Writer which do stream-based, forward-only XML parsing and creation.
+  * XmlLite is a native C++ implementation of .NET XmlReader+Writer for stream-based, forward-only XML parsing and creation.
   * XmlLite.dll is required.  It is included with all new versions of Windows, and service packs for old versions.
   * XmlReader's pull-based interface is cleaner to use than SAX's event-based interface.
   * More info: http://msdn.microsoft.com/en-us/library/ms752838%28v=VS.85%29.aspx
